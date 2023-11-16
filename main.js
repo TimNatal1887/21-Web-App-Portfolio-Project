b4f226848ee21bad5f7b490f646f2ab1c7c09517
Timothy Natal <timothynatal@pursuit.org>
2023-11-15 19:14:53 -0500

    Display mostly done, added basic functionality to start game button

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const wagerForm = document.querySelector(".wager")
+const cardLis = ""
+const drawButton = document.querySelector(".draw")
+const holdButton = document.querySelector(".hold")
 
+
+
+wagerForm.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    drawButton.style.display = "inline"
+    holdButton.style.display = "inline"
+
+
+
+})
+
+
+function displayCard(card){
+    const newCard = document.createElement("li");
+    newCard.inner
+//     <li class="card two">
+//     <img src="assets/examplecard.png" alt="ace-of-clubs">
+// </li>
+}
