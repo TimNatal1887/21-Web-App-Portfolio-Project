b39af1bca239dc52c002ba838df419079f20af91
Timothy Natal <timothynatal@pursuit.org>
2023-11-15 23:48:02 -0500

    Added basic setup for event listeners and beginnings of button functionality

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
 const wagerForm = document.querySelector(".wager")
 const cardLis = ""
+const ruleText = document.querySelector(".default")
+const restartButton = document.querySelector(".reset")
 const drawButton = document.querySelector(".draw")
 const holdButton = document.querySelector(".hold")
 
-
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/shuffle/")
+    .catch((error)=> console.log(error))
+})
 
 wagerForm.addEventListener("submit",(event)=>{
     event.preventDefault()
-    drawButton.style.display = "inline"
-    holdButton.style.display = "inline"
-
-
-
+    startGame()
+    
+    
+    
+    
+})
+holdButton.addEventListener("submit",(event)=>{
+    event.preventDefault()
 })
 
+holdButton.addEventListener("submit",(event)=>{
+    event.preventDefault()    
+})
+
+restartButton.addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+function startGame(){
+    const points = document.querySelector("#points").value
+    const wagerAmount = document.querySelector(".wager-amount")
+
+    drawButton.style.display = "inline"
+    holdButton.style.display = "inline"
+    wagerForm.style.display ="none"
+    restartButton.style.display = "inline"
+    ruleText.style.display = "inline"
+    wagerAmount.textContent = `Wager Amount : ${points}`
+
+
+
+}
 
 function displayCard(card){
     const newCard = document.createElement("li");
-    newCard.inner
-//     <li class="card two">
-//     <img src="assets/examplecard.png" alt="ace-of-clubs">
-// </li>
+    newCard.classList.add("card");
+    newCard.innerHTML = `<img src="${card.image}" alt="">`
 }
