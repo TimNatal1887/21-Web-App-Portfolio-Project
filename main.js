9cd816c0c3d68b13d9882e94732527fbdfab688d
Timothy Natal <timothynatal@pursuit.org>
2023-11-16 20:17:53 -0500

    Points added function partially working

--- main.js
+++ main.js
@@ -1,30 +1,110 @@
 const dealerHand = document.querySelector("#dealers-count");
 const playerHand = document.querySelector("#players-count");
 const wagerForm = document.querySelector(".wager")
-const cardList = document.querySelector(".player-hand")
 const ruleText = document.querySelector(".default")
 const restartButton = document.querySelector(".reset")
 const drawButton = document.querySelector(".draw")
 const holdButton = document.querySelector(".hold")
 const winMessage = document.querySelector(".win-message")
+const errorMessage = document.querySelector(".error")
+let points = 0
 
 document.addEventListener("DOMContentLoaded",()=>{
     fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/shuffle/")
     .catch((error)=> console.log(error))
-})
 
-wagerForm.addEventListener("submit",(event)=>{
-    event.preventDefault()
-    fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/draw/?count=2")
-    .then((response) => response.json())
-    .then((data)=> {
-        data.cards.forEach(card => displayCard(card))
-        startGame()
+    wagerForm.addEventListener("submit",(event)=>{
+        event.preventDefault()
+        // if(!wagerForm.checkValidity()){
+        //     errorMessage.style.display = "block"
+        // }else{
+        //     errorMessage.style.display = "none"
+        // }
+        fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/draw/?count=2")
+        .then((response) => response.json())
+        .then((data)=> {
+            data.cards.forEach(card => displayCard(playerHand,"player",card))
+            startGame()
+        })
+    
+        fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/draw/?count=2")
+        .then((response) => response.json())
+        .then((data)=> {
+            data.cards.forEach(card => displayCard(dealerHand,"dealer",card))
+        })
     })
+    holdButton.addEventListener("submit", handleHoldButton);
+    restartButton.addEventListener("submit", handleRestartButton)
     
 })
+function handleHoldButton(event){
+    event.preventDefault();
+    let foundWinner = "";
+    let dealersCount = parseInt(dealerHand.textContent)
+    let playersCount = parseInt(playerHand.textContent)
+    drawButton.style.display = "none";
+    holdButton.style.display = "none";
+    const playerTurnOver = true;
+    if(dealersCount >= 17){
+        foundWinner = checkWinner(dealersCount,playersCount,playerTurnOver,true)
+    }else{
+        foundWinner = checkWinner(dealersCount,playersCount,playerTurnOver,false)
+    }
 
-drawButton.addEventListener("click", (event) => {
+    if(foundWinner){
+        isWinnerFound(foundWinner)
+    }else{
+        const intervalId = setInterval(() => {
+            dealersCount = dealerDraws(dealersCount, playersCount, playerTurnOver, intervalId);
+        }, 2000);
+
+    }
+}
+
+
+    function handleRestartButton(event){
+        event.preventDefault();
+        fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/shuffle/")
+        resetPage();
+}
+
+function dealerDraws(count1, count2, playerTurnOver, intervalId) {
+    fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/draw/?count=1")
+        .then((response) => response.json())
+        .then((data) => {
+            displayCard(dealerHand, "dealer", data.cards[0]);
+            count1 = cardCounter(dealerHand, document.querySelectorAll(".dealer"));
+            document.querySelector("#dealers-count").textContent = count1;
+            const foundWinner = checkWinner(count1, count2, playerTurnOver, false);
+            if(foundWinner){
+                isWinnerFound(foundWinner);
+
+            }
+            if (count1 < 21 && count1 < count2 && count1 < 17) {
+                return count1
+            } else {
+                clearInterval(intervalId);
+                const finalWinner = checkWinner(count1,count2, playerTurnOver, true);
+                isWinnerFound(finalWinner);
+            }
+    });
+}
+
+
+
+function startGame(){
+    const wagerAmount = document.querySelector("#wager-amount")
+    const points = document.querySelector("#points").value
+    drawButton.style.display = "inline"
+    holdButton.style.display = "inline"
+    wagerForm.style.display ="none"
+    restartButton.style.display = "inline"
+    ruleText.style.display = "inline"
+    wagerAmount.textContent = `${points}`   
+    drawButton.addEventListener("click",handleDrawButton);   
+}
+
+function handleDrawButton(event){
     event.preventDefault();
     let dealersCount = parseInt(dealerHand.textContent)
     let playersCount = parseInt(playerHand.textContent)
@@ -32,127 +112,86 @@ drawButton.addEventListener("click", (event) => {
     fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/draw/?count=1")
     .then((response) => response.json())
     .then((data) => {
-        displayCard(data.cards[0]);
-        playersCount = cardCounter(document.querySelectorAll(".card"));
+        displayCard(playerHand,"player",data.cards[0]);
+        playersCount = cardCounter("player",document.querySelectorAll(".player"));
         foundWinner = checkWinner(dealersCount, playersCount, false, false);
         isWinnerFound(foundWinner);
-    });
-});
-
-
-holdButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let dealersCount = parseInt(dealerHand.textContent)
-    let playersCount = parseInt(playerHand.textContent)
-    let foundWinner = "";
-    drawButton.style.display = "none";
-    holdButton.style.display = "none";
-    const playerTurnOver = true;
-    foundWinner = checkWinner(dealersCount,playersCount,playerTurnOver,false)
-    if(foundWinner){
-        isWinnerFound(foundWinner)
-    }else{
-        const intervalId = setInterval(() => {
-            dealersCount = dealerDraws(dealersCount, playersCount, playerTurnOver, intervalId);
-        }, 2000);
-    
-    }
-});
-
-restartButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    fetch("https://deckofcardsapi.com/api/deck/fmlxy9qw29b8/shuffle/")
-    resetPage();
-    
-});
-
-function dealerDraws(count1, count2, playerTurnOver, intervalId) {
-    count1 += Math.ceil(Math.random() * 10);
-    foundWinner = checkWinner(count1, count2, playerTurnOver, false);
-    console.log(foundWinner)
-    document.querySelector("#dealers-count").textContent = count1;
-
-    if (count1 < 21 && count1 < count2) {
-        return count1;
-    } else {
-        clearInterval(intervalId);
-        foundWinner = checkWinner(count1, count2, playerTurnOver, true);
-        isWinnerFound(foundWinner)
-        console.log(foundWinner)
-        return count1;
-    }
+    })
 }
 
-
-
-function startGame(){
-    const points = document.querySelector("#points").value
-    const wagerAmount = document.querySelector(".wager-amount")
-    const dealerHand = document.querySelector("#dealers-count")
-    const dealerCount = Math.ceil(Math.random() * 21)
-    drawButton.style.display = "inline"
-    holdButton.style.display = "inline"
-    wagerForm.style.display ="none"
-    restartButton.style.display = "inline"
-    ruleText.style.display = "inline"
-    wagerAmount.textContent = `Wager Amount : ${points}`
-    dealerHand.textContent = `${dealerCount}`      
-}
-
-
-
-function displayCard(card){
+function displayCard(hand,player,card){
+    const cardList = document.querySelector(`.${player}-hand`)
     const newCard = document.createElement("li");
     newCard.classList.add("card");
+    newCard.classList.add(player)
     newCard.id = `${card.value}`
     newCard.innerHTML = `<img src="${card.image}" alt="">`
     cardList.append(newCard)
-    const allCards = document.querySelectorAll(".card")
-    cardCounter(allCards)  
+    const allCards = document.querySelectorAll(`.${player}`)
+    cardCounter(hand,allCards)  
 }
 
-function cardCounter(cards){
-    const playerHand = document.querySelector("#players-count")
+function cardCounter(hand,cards){
     let aceCount = 0
-    let playerCount = 0
+    let cardCount = 0
     cards.forEach(card=> {
         const cardValue = card.id
         if(cardValue === "KING" || cardValue === "QUEEN" || cardValue === "JACK"){
-            playerCount += 10
+            cardCount += 10
         }else if(cardValue === "ACE"){
             aceCount++
-            playerCount += 11
+            cardCount += 11
             
         }else{
-            playerCount += +cardValue
+            cardCount += +cardValue
         }
 
     })
     
-    while(playerCount > 21 && aceCount > 0){
-        playerCount -= 10
+    while(cardCount > 21 && aceCount > 0){
+        cardCount -= 10
         aceCount--
         
     }
     
-    playerHand.textContent = `${playerCount}`
-    return playerCount
+    hand.textContent = `${cardCount}`
+    return cardCount
 
 }
 
-function checkWinner(count1,count2,playerTurnOver,dealerTurnOver){
-    let winner = ""
-    if((count2 > count1 && count2 <= 21 && dealerTurnOver) || (count2 <= 21 && count1 > 21)){
-        winMessage.classList.add("player-won")
-        winMessage.textContent = "Player Wins!"
-        winner = 'Player'
-    }else if((count2 <= count1 && count1 <= 21 && playerTurnOver) || (count2 > 21 && count1 <= 21)){
-        winMessage.classList.add("dealer-won")
-        winMessage.textContent = "Dealer Wins!"
-        winner = 'Dealer'
+function checkWinner(count1, count2, playerTurnOver, dealerTurnOver){
+    const pointsWon = document.querySelector("#points-won");
+    const pointsLost = document.querySelector("#points-lost");
+    let points = parseInt(document.querySelector("#wager-amount").textContent)
+    let playerWonPoints = parseInt(pointsWon.textContent);
+    let playerPointsLost = parseInt(pointsLost.textContent);
+    let winner = "";
+    if ((count2 > count1 && count2 <= 21 && dealerTurnOver) || (count2 <= 21 && count1 > 21)) {
+        winMessage.classList.add("player-won");
+        winMessage.textContent = "Player Wins!";
+        winner = 'Player';
+        points *=2
+        playerWonPoints += points
+        pointsWon.textContent = `${playerWonPoints}`
+
+
+    } else if ((count2 <= count1 && count1 <= 21 && playerTurnOver) || (count2 > 21 && count1 <= 21)) {
+        winMessage.classList.add("dealer-won");
+        winMessage.textContent = "Dealer Wins!";
+        winner = 'Dealer';
+        points *=2
+        playerPointsLost += points
+        pointsLost.textContent = `${playerPointsLost}`
     }
-    return winner
+    // console.log(playerPointsLost)
+    // console.log(playerWonPoints)
+    // console.log(points)
+    // console.log(winner)
+    return winner;
 }
+
+
+
 
 function isWinnerFound(foundWinner){
     if(foundWinner){
@@ -167,8 +206,7 @@ function resetPage(){
     const cardList = document.querySelectorAll(".card")
     const dealerHand = document.querySelector("#dealers-count");
     const playerHand = document.querySelector("#players-count");
-    const wagerAmount = document.querySelector(".wager-amount");
-
+    const wagerAmount = document.querySelector("#wager-amount");
     ruleText.style.display = "none"
     drawButton.style.display = "none"
     holdButton.style.display = "none"
@@ -178,7 +216,11 @@ function resetPage(){
     cardList.forEach(card=> card.remove())
     dealerHand.textContent = ""
     playerHand.textContent = ""
-    wagerAmount.textContent = ""
+    wagerAmount.textContent = "0"
     restartButton.style.display = "none"
 
+    // drawButton.removeEventListener("submit", handleDrawButton);
+    // holdButton.removeEventListener("submit",handleHoldButton);
+    // restartButton.removeEventListener("click", handleRestartButton);
+    wagerForm.reset()
 }
